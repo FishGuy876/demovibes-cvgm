d629e7ac183571cdb5a577f1ff4d8fd153c9d6a4
Andy Kellett <fishguy8765@gmail.com>
2021-02-22 16:51:54 -0500

    Added demosauce Startup Script

---
+++ demosauce/start_demosauce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# A bash script to start Demosauce, and re-start it in the event it stops. Depending on the system, it can take up
+# To 60 seconds to restart again and skip several songs in the process. Increase the sleep delay to prevent the
+# Number of skipped songs, as the rest of the system catches up and restarts.
+
+while /bin/true
+do
+	echo "Restarting Demosauce"
+	./demosauce -c demosauce.conf
+
+	# Pause briefly before restart
+	echo "Pausing Briefly"
+	sleep 10
+done
