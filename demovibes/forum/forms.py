e61165b2659e50ce3113b539d35b8294e4d9f1e6
Andy Kellett <fishguy8765@gmail.com>
2021-08-17 15:32:14 -0400

    Forum Cleanup, Added Rules For Posting Template

--- demovibes/forum/forms.py
+++ demovibes/forum/forms.py
@@ -5,29 +5,29 @@ class ThreadForm(ModelForm):
     class Meta:
         model = Thread
         fields = ['title']
-    title = CharField(required = True, label = 'Subject')
-    body = CharField(required = True, label = 'Message',
+    title = CharField(required = True, label = 'Topic Name')
+    body = CharField(required = True, label = 'Topic Contents',
         widget = Textarea(attrs = {'rows': '12', 'cols': '80',
             'class': 'input'}))
     subscribe = BooleanField(required = False,
-        label='Receive updates via email')
+        label='Receive Email When Someone Replies')
 
 
 class ReplyForm(ModelForm):
     class Meta:
         model = Post
         fields = ['body']
-    body = CharField(required = True, label = 'Your Message',
+    body = CharField(required = True, label = 'Your Reply',
         widget = Textarea(attrs = {'rows': '12', 'cols': '80',
             'class': 'input'}))
     subscribe = BooleanField(required = False,
-        label='Receive updates via email')
+        label='Receive Email When Someone Replies')
 
 class EditForm(ModelForm):
     class Meta:
         model = Post
         fields = ['body']
-    body = CharField(required = True, label = 'Your Message',
+    body = CharField(required = True, label = 'Your Reply',
     widget = Textarea(attrs = {'rows': '20', 'cols': '60',
         'class': 'input'}))
 
