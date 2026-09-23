997afebae4da46cab850db50a02d8389a2bef40e
Andy Kellett <fishguy8765@gmail.com>
2026-09-22 20:33:35 -0400

    Convert demosauce to git submodule from FishGuy876/demosauce
    Replaces the plain in-tree demosauce copy with a submodule pinned to
    the fork, so future updates go through git submodule update instead
    of direct commits to demovibes-cvgm.

--- demosauce/contrib/test-scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# runs scan on a bunch of files that caused problems in the past.
-# build with ASan to find memory bugs
-
-for FILE in "$@"; do
-    echo "$FILE"
-    ./dscan "$FILE" -cr || echo "test failed"
-done
--- demosauce/libs/openmpt/build/dist.mk
+++
@@ -1,4 +0,0 @@
-
-MPT_SVNVERSION=10633
-MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.3.11
-MPT_SVNDATE=2018-07-28T11:50:00.062292Z
